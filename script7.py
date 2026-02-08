4d231688b586546e34b2dcce70b366174b8212fc
Maryam_waheed <dsa35524@gmail.com>
2026-02-08 19:23:35 +0300

    Add files via upload

---
+++ script7.py
@@ -0,0 +1,10 @@
+#lab3 Question 2
+age=int(input('Enter a number'))
+if age<13:
+    print('Child')
+elif age >=19:
+    print('Teen')
+elif age >=59:
+    print('Adult')
+else:
+    print('Senior')
