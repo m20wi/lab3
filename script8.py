d801ae84a7994d38cb61b2abeabf7fe6a0361c5f
Maryam_waheed <dsa35524@gmail.com>
2026-02-08 19:24:06 +0300

    q3

---
+++ script8.py
@@ -0,0 +1,10 @@
+#lab3 Question 3
+num=(int(input('Enter a number:')))
+if num >0:
+    print('positive')
+else:
+    print('not positive')
+if 10<=num<=50:
+    print('the number is between 10 and 50')
+else:
+        print('the number is not between 10 and 50')
